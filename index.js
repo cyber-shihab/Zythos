03500e83ffd8423889b7e618c339f0d18ebd4e74
cyber-shihab <itsmeshihabuddin@gmail.com>
2026-06-09 19:47:19 +0600

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,14 @@
 const { Client, GatewayIntentBits } = require('discord.js');
-
 const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent] });
 
 client.on('ready', () => {
-    console.log(`Logged in as ${client.user.tag}`);
+  console.log(`Logged in as ${client.user.tag}!`);
 });
 
 client.on('messageCreate', msg => {
-    if (msg.content === 'ping') {
-        msg.reply('Pong!');
-    }
+  if (msg.content === 'ping') {
+    msg.reply('pong');
+  }
 });
 
-client.login(MTUxMzg2Mzg0Njc3Nzk3OTAyMQ.Gj5KmG.EqzuW7q4nQ6FJ_NckCK7__AWNDL4KTLvU1GFxg');
+client.login('MTUxMzg2Mzg0Njc3Nzk3OTAyMQ.Gj5KmG.EqzuW7q4nQ6FJ_NckCK7__AWNDL4KTLvU1GFxg');
